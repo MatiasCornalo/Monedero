9fe9e1fdc51b380bab0de975052596e2cec3991f
MatiasCornalo <130251779+MatiasCornalo@users.noreply.github.com>
2024-04-29 13:49:07 -0300

    Initialize refactor-2023-viernes

---
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -0,0 +1,86 @@
+package dds.monedero.model;
+
+import dds.monedero.exceptions.MaximaCantidadDepositosException;
+import dds.monedero.exceptions.MaximoExtraccionDiarioException;
+import dds.monedero.exceptions.MontoNegativoException;
+import dds.monedero.exceptions.SaldoMenorException;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cuenta {
+
+  private double saldo = 0;
+  private List<Movimiento> movimientos = new ArrayList<>();
+
+  public Cuenta() {
+    this(0);
+  }
+
+  public Cuenta(double montoInicial) {
+    saldo = montoInicial;
+  }
+
+  public void setMovimientos(List<Movimiento> movimientos) {
+    this.movimientos = movimientos;
+  }
+
+  public void poner(double cuanto) {
+    validarMontoNegativo(cuanto);
+
+    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+      throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
+    }
+
+    new Deposito(LocalDate.now(), cuanto).agregateA(this);
+  }
+
+  public void sacar(double cuanto) {
+    validarMontoNegativo(cuanto);
+    validarExtraccion(cuanto);
+    new Extraccion(LocalDate.now(), cuanto).agregateA(this);
+  }
+
+  private void validarExtraccion(double cuanto) {
+    if (getSaldo() - cuanto < 0) {
+      throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
+    }
+    double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
+    double limite = 1000 - montoExtraidoHoy;
+    if (cuanto > limite) {
+      throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
+          + " diarios, límite: " + limite);
+    }
+  }
+
+  private void validarMontoNegativo(double cuanto) {
+    if (cuanto <= 0) {
+      throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
+    }
+  }
+
+  public void agregarMovimiento(Movimiento movimiento) {
+    movimientos.add(movimiento);
+  }
+
+  public double getMontoExtraidoA(LocalDate fecha) {
+    return getMovimientos().stream()
+        .filter(movimiento -> movimiento.getFecha().equals(fecha))
+        .mapToDouble(Movimiento::getMontoExtraccion)
+        .sum();
+  }
+
+  public List<Movimiento> getMovimientos() {
+    return movimientos;
+  }
+
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public void setSaldo(double saldo) {
+    this.saldo = saldo;
+  }
+
+}
